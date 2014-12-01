20dc40ac90501df8a1bfe957e8836c9be2178d85
raysango <remongeorge@live.com>
2014-12-01 04:14:17 +0000

    SMTP update for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,7 +72,11 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-
+  
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  config.action_mailer.default_url_options = { host: 'https://raybloccit.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
 end
