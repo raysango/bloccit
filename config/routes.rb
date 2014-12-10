2ac2a6485742884381853bd56fafd2058dbe4166
raysango <remongeorge@live.com>
2014-12-10 07:29:08 +0000

    shallow-nesting

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,10 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:update]
   resources :topics do
-    resources :posts, except: [:index] do
+    resources :posts, except: [:index]
+  end
+  resources :posts, only: [] do
       resources :comments, only: [:create, :destroy]
-    end
   end
 
   get 'about' => "welcome#about"
