9b19d19275b56cf1d94b2744e1557dbaa2983fbe
raysango <remongeorge@live.com>
2014-12-19 02:09:19 +0000

    Votes controller update

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -21,7 +21,7 @@ class VotesController < ApplicationController
   def update_vote(new_value)
     if @vote
       authorize @vote, :update?
-      @vote.update_attributes(:value, new_value)
+      @vote.update_attribute(:value, new_value)
     else
       @vote = current_user.votes.build(value: new_value, post: @post)
       authorize @vote, :create?
